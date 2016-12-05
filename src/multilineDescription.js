08eefe5a889fd31f908867b7b46780a200b38da8
Chris Nordhougen <cwnord@gmail.com>
2016-12-05 11:25:30 -0600

    mld needed a space for newline replacements

--- src/multilineDescription.js
+++ src/multilineDescription.js
@@ -1,4 +1,4 @@
 export default function multilineDescription(jasmine) {
-    jasmine.mld = strs => strs[0].trim().replace(/\s{2,}/g, '');
+    jasmine.mld = strs => strs[0].trim().replace(/\s{2,}/g, ' ');
     return jasmine;
 }
